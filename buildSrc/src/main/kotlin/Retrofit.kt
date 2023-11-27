efa1935587751f5217ce60740a3a2eeae20545d4
Aveek <nasrat-sharif.aveek@electrolux.com>
2023-11-27 23:06:30 +0800

    Update Config file

---
+++ buildSrc/src/main/kotlin/Retrofit.kt
@@ -0,0 +1,8 @@
+object Retrofit {
+    const val retrofitVersion = "2.9.0"
+    const val okhttpLoggingInterceptor_version = "4.9.0"
+    const val retrofitCore = "com.squareup.retrofit2:retrofit:$retrofitVersion"
+    const val retrofitConverter = "com.squareup.retrofit2:converter-gson:$retrofitVersion"
+    const val retrofitMock = "com.squareup.retrofit2:retrofit-mock:$retrofitVersion"
+    const val retrofitInterceptor = "com.squareup.okhttp3:logging-interceptor$okhttpLoggingInterceptor_version"
+}
