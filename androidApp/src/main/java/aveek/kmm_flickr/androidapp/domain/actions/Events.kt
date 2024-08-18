efa2cb1724a75d364d02cf60f8bf76a781389b7d
Aveek <nsaveek@gmail.com>
2024-08-18 18:12:29 +0800

    Updated Events file as directed in MVI pattern

--- androidApp/src/main/java/aveek/kmm_flickr/androidapp/domain/actions/Events.kt
+++ androidApp/src/main/java/aveek/kmm_flickr/androidapp/domain/actions/Events.kt
@@ -1,7 +1,7 @@
 package aveek.kmm_flickr.androidapp.domain.actions
 
-sealed class Events{
-    data class search (val query : String) : Events()
-    data class selectListItem (val index : Int) : Events()
-    data class downloadSelectedListItem (val index : Int, val mediaUrl : String) : Events()
+sealed interface Events{
+    data class search (val query : String) : Events
+    data class selectListItem (val index : Int) : Events
+    data class downloadSelectedListItem (val index : Int, val mediaUrl : String) : Events
 }
